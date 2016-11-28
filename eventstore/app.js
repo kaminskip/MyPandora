444028b264974f6d4a672cefe34452bf5697af7d
Pawel Kaminski <kaminskip@gmail.com>
2016-11-28 15:58:36 +0100

    Add logging

--- eventstore/app.js
+++ eventstore/app.js
@@ -2,16 +2,18 @@ var mqtt = require('mqtt');
 var config = require('./config.json');
 var mapEvent = require('./mapEvent.js');
 
-console.log("Connecting to MQTT server " + config.mqtt_url);
+console.log("Connecting to MQTT server " + config.mqtt_url + " ...");
 var client = mqtt.connect(config.mqtt_url);
 
 client.on('connect', function () {
+    console.log("Connected!");
     console.log("Subscribe on " + config.topic.salonTemp);
     client.subscribe(config.topic.salonTemp);
     console.log("Waiting for events ...");
-});
 
-client.on('message', function (topic, message) {
-    var msg = mapEvent.tempMsg(topic, message);
-    console.log(msg);
+    client.on('message', function (topic, message) {
+        var msg = mapEvent.tempMsg(topic, message);
+        console.log(msg)
+    });
+
 });
