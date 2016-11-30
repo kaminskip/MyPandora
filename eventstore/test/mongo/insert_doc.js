c1060be2319844482e71d8f6440d22b861726966
Pawel Kaminski <kaminskip@gmail.com>
2016-11-30 10:17:39 +0100

    Change mongo test files

--- eventstore/test/mongo/insert_doc.js
+++ eventstore/test/mongo/insert_doc.js
@@ -1,29 +1,11 @@
-var insertDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-};
+var config = require("../config.json");
+var TestFun = require("./functions");
 
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
 
-// Connection URL
-var url = 'mongodb://localhost:27017/test';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
+MongoClient.connect(config.mongo_url, function(err, db) {
     console.log("Connected successfully to server");
-
-    insertDocuments(db, function() {
+    TestFun.insertDocuments(db, function() {
         db.close();
     });
 });
--- eventstore/test/mongo/update.js
+++ eventstore/test/mongo/update.js
@@ -1,27 +1,11 @@
-var updateDocument = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Update document where a is 2, set b equal to 1
-    collection.updateOne({ a : 2 }
-        , { $set: { b : 1 } }, function(err, result) {
-            assert.equal(err, null);
-            assert.equal(1, result.result.n);
-            console.log("Updated the document with the field a equal to 2");
-            callback(result);
-        });
-};
+var config = require("../config.json");
+var TestFun = require("./functions");
 
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
 
-
-// Connection URL
-var url = 'mongodb://localhost:27017/test';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
+MongoClient.connect(config.mongo_url, function(err, db) {
     console.log("Connected correctly to server");
-    updateDocument(db, function() {
+    TestFun.updateDocument(db, function() {
         db.close();
     });
 });
