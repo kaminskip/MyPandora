9a3f00695ef3b5e40800cbcf326ce6b8f2b16a9f
Pawel Kaminski <kaminskip@gmail.com>
2018-03-10 19:56:32 +0100

    Test info

--- gate/server.js
+++ gate/server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8888;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -19,7 +19,15 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'Pandora gateway api' });
+});
+
+router.get('/home/salon/temp', function(req, res) {
+    res.json({ value: '23' });
+});
+
+router.get('/home/bedroom/temp', function(req, res) {
+    res.json({ value: '25.5' });
 });
 
 // more routes for our API will happen here
@@ -31,4 +39,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Start server on port ' + port);
+console.log('Pandora API gateway started on ' + port);
